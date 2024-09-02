b0fd16c8208729b4c245eb4f38e1a9853d41b07c
Amiiacca <santaamaitsu@gmail.com>
2024-09-02 13:13:41 -0600

    Change player speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left","right","up","down")
-	velocity = direction * 100
+	velocity = direction * 300
 	
 	var x_dir = Input.get_axis("left","right")
 	
